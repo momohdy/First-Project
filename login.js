dfb925f55e68e85875c46f441ab75a25a3848ca0
Mostafa Mohdy <103789273+momohdy@users.noreply.github.com>
2022-10-15 11:27:35 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+
+
+// let userInput = document.getElementById("user");
+// let passInput = document.getElementById("password");
+// let sub =document.getElementById("sub")
+
+// sub.onclick = function (){
+
+// }
+// sub.onsubmit = function (e) {
+//     let userValid = false;
+//     let  = false;
+//     if(userInput.value !=== "" && userInput.value <=10){
+//         userValid=true;
+//     }
+//     if(passInput.value !== "")
+
+
+//     if(userValid === false || ageValid === false){
+//         e.preventDefault()
+//     }
+// }
